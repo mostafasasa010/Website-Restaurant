c75a70ecd9ee10554acb3d72e84fa7787fccb71d
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-01-31 02:40:01 +0200

    Finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,28 @@ navLinks.forEach((link) => {
     navMenu.classList.toggle("active");
   });
 });
+
+const changeTheme = document.querySelector(".change-theme");
+
+const savedTheme = window.localStorage.getItem("theme");
+if (savedTheme) {
+  document.body.classList.toggle("dark-theme", savedTheme === "moon");
+  changeTheme.classList.toggle("bx-sun", savedTheme === "sun");
+}
+
+changeTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  const isDarkTheme = document.body.classList.contains("dark-theme");
+  window.localStorage.setItem("theme", isDarkTheme ? "moon" : "sun");
+  changeTheme.classList.toggle("bx-sun", !isDarkTheme);
+});
+
+const btnTop = document.querySelector(".btn-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 500) {
+    btnTop.classList.add("active");
+  } else {
+    btnTop.classList.remove("active");
+  }
+});
